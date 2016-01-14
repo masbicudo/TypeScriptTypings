af12521602876e16f5a107470eba1f6af0dd32f7
masbicudo <masbicudo@gmail.com>
2016-01-14 18:07:02 -0200

    object-assign.d.ts

--- object-assign/object-assign.d.ts
+++ object-assign/object-assign.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for object-assign 4.0.1
 // Project: https://github.com/sindresorhus/object-assign
 // Definitions by: Miguel Angelo <https://github.com/masbicudo>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
+// Definitions: https://github.com/masbicudo/DefinitelyTyped
 
 declare module "object-assign" {
     export function objectAssign(target: any, ...sources: any[]): any;
