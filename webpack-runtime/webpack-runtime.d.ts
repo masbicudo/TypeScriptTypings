129d326baf5e1277a573ea48b0f1ca422e7a01a3
masbicudo <masbicudo@gmail.com>
2016-01-30 13:12:21 -0200

    webpack-runtime.d.ts - I had to change the syntax because compiler complains

--- webpack-runtime/webpack-runtime.d.ts
+++ webpack-runtime/webpack-runtime.d.ts
@@ -2,16 +2,17 @@
  * Type declarations for Webpack runtime.
  * From: https://github.com/andreypopp/typescript-loader
  *              /blob/master/lib/webpack-runtime.d.ts
- * Author: andreypopp  https://github.com/andreypopp
+ * Authors: BASE: andreypopp  https://github.com/andreypopp
+ *          CHANGES: masbicudo - I had to change the syntax because compiler complains
  */
 
 interface WebpackRequireEnsureCallback {
-  ((id: string) => any) => any
+    (fn: (id: string) => any): any;
 }
 
 interface WebpackRequire {
-  (id: string) => any;
-  ensure(ids: string[], WebpackRequireEnsureCallback) => any;
+  (id: string): any;
+  ensure(ids: string[], WebpackRequireEnsureCallback): any;
 }
 
 declare var require: WebpackRequire;
