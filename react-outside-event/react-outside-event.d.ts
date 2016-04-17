549008af615e0e31177956a337a9cffd92c8f89c
masbicudo <masbicudo@gmail.com>
2016-04-16 23:59:01 -0300

    react-outside-event.d.ts (corrections)

--- react-outside-event/react-outside-event.d.ts
+++ react-outside-event/react-outside-event.d.ts
@@ -2,12 +2,12 @@
 // Project: https://github.com/gajus/react-outside-event
 // Definitions by: Miguel Angelo
 // Definitions: https://github.com/masbicudo/TypeScriptTypings
-
 /**
  * Created by Miguel Angelo on 16/04/2016.
  */
 declare module "react-outside-event" {
     import * as React from 'react';
     class ElementClass extends React.Component<any, any> { }
-    export default function fn<T extends (typeof ElementClass)>(targetComponentClass: T, eventNames?: string[]): T;
+    function fn<T extends (typeof ElementClass)>(targetComponentClass: T, eventNames?: string[]): T;
+    export = fn;
 }
